646246a425203281453e83f99748626d8f14c67d
Alvaro <alvaro347@gmail.com>
2018-03-11 23:05:51 +0000

    replaced enemy function to a class that contains all functions

--- js/app.js
+++ js/app.js
@@ -1,42 +1,67 @@
 /*jshint esversion: 6 */
 
 
-// Enemies our player must avoid
-var Enemy = function(x, y, speed) {
-  // Variables applied to each of our instances go here,
-  // we've provided one for you to get started
-  this.x= x;
-  this.y= y;
-  this.speed = speed;
-  // The image/sprite for our enemies, this uses
-  // a helper we've provided to easily load images
-  this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-  // You should multiply any movement by the dt parameter
-  // which will ensure the game runs at the same speed for
-  // all computers.
-  this.x = this.x + (dt * this.speed);
-  if (this.x > 500) {
-    this.x = -100;
-    this.speed = 200 + Math.random() * 300;
+class Enemy {
+  constructor(x, y, speed) {
+    this.x= x;
+    this.y= y;
+    this.speed = speed;
+    this.sprite = 'images/enemy-bug.png';
   }
-  if (player.x - this.x < 50 && player.x - this.x > -50) {
-    if (player.y - this.y < 30 && player.y - this.y > -30) {
-      player.x = 200;
-      player.y = 380;
+  update(dt) {
+    this.x = this.x + (dt * this.speed);
+    if (this.x > 500) {
+      this.x = -100;
+      this.speed = 200 + Math.random() * 300;
     }
-
+    if (player.x - this.x < 50 && player.x - this.x > -50) {
+      if (player.y - this.y < 30 && player.y - this.y > -30) {
+        player.x = 200;
+        player.y = 380;
+      }
+    }
   }
-};
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+}
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+// Enemies our player must avoid
+// var Enemy = function(x, y, speed) {
+//   // Variables applied to each of our instances go here,
+//   // we've provided one for you to get started
+//   this.x= x;
+//   this.y= y;
+//   this.speed = speed;
+//   // The image/sprite for our enemies, this uses
+//   // a helper we've provided to easily load images
+//   this.sprite = 'images/enemy-bug.png';
+// };
+//
+// // Update the enemy's position, required method for game
+// // Parameter: dt, a time delta between ticks
+// Enemy.prototype.update = function(dt) {
+//   // You should multiply any movement by the dt parameter
+//   // which will ensure the game runs at the same speed for
+//   // all computers.
+//   this.x = this.x + (dt * this.speed);
+//   if (this.x > 500) {
+//     this.x = -100;
+//     this.speed = 200 + Math.random() * 300;
+//   }
+//   if (player.x - this.x < 50 && player.x - this.x > -50) {
+//     if (player.y - this.y < 30 && player.y - this.y > -30) {
+//       player.x = 200;
+//       player.y = 380;
+//     }
+//
+//   }
+// };
+//
+// // Draw the enemy on the screen, required method for game
+// Enemy.prototype.render = function() {
+//   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+// };
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -53,7 +78,6 @@ class Player {
       this.x = 200;
       this.y = 380;
       this.score += 1;
-      
     }
   }
   render() {
@@ -80,10 +104,8 @@ let allEnemies = [];
 let y = [225,145,65];
 
 y.forEach(function (y) {
-  for (var i = 0; i < 1; i++) {
-    let enemy = new Enemy(-100, y, 200 + Math.random() * 300);
-    allEnemies.push(enemy);
-  }
+  let enemy = new Enemy(-100, y, 200 + Math.random() * 300);
+  allEnemies.push(enemy);
 });
 
 
@@ -98,6 +120,5 @@ document.addEventListener('keyup', function(e) {
     39: 'right',
     40: 'down'
   };
-
   player.handleInput(allowedKeys[e.keyCode]);
 });
